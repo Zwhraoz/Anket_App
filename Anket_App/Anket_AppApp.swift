ae70ed861ba30dbcd90595e5e48eb4ae0b902611
Zehra Özer <ozerzwhra@gmail.com>
2025-05-18 14:27:52 +0300

    Initial Commit

---
+++ Anket_App/Anket_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anket_AppApp.swift
+//  Anket_App
+//
+//  Created by zehra özer on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Anket_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
